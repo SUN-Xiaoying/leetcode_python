5364f8f0830503f12ab436e9e3d527fa3833a901
SUN-Xiaoying <xiaoyingsun523@gmail.com>
2026-01-09 15:18:27 +0100

    20260109 all sub strings

--- dp/string/all_sub_string.py
+++ dp/string/all_sub_string.py
@@ -2,5 +2,25 @@ from typing import List
 
 
 class Solution:
-    def allSubString(self, parent: str) -> List[str]:
-        
+    def allSubString(self, s: str) -> List[str]:
+        result = set()
+        path = ""
+
+        def dfs(s: str, i: int) :
+            nonlocal path
+            if i >= len(s) :
+                result.add(path)
+            else:
+                path += s[i]
+                dfs(s, i + 1)
+                path = path[:-1]
+                dfs(s, i + 1)
+
+        dfs(s, 0)
+
+
+        return list(result)
+
+
+s = Solution()
+print(s.allSubString("abbc"))
---
+++ dp/string/longest_nice_substring.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/longest-nice-subarray/description/
+from typing import List
+
+
+class Solution:
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+
