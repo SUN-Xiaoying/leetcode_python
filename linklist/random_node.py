e0b6543c11f3742afb6443639f3e9be44a46ba9f
SUN-Xiaoying <xiaoyingsun523@gmail.com>
2026-09-20 11:43:00 +0200

    20260920 linked list random node

--- linklist/random_node.py
+++ linklist/random_node.py
@@ -1,11 +1,20 @@
 # https://leetcode.com/problems/linked-list-random-node/description
+import random
 from typing import Optional
 
 from linklist.helper import ListNode
 
-
+# 8ms Beats 84.36%
 class Solution:
 
     def __init__(self, head: Optional[ListNode]):
+        self.arr = []
+        self.size = 0
+        current = head
+        while current is not None:
+            self.arr.append(current.val)
+            self.size += 1
+            current = current.next
 
     def getRandom(self) -> int:
+        return self.arr[random.randrange(0, self.size)]
